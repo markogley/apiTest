1c9eda66f0bb8d335505142822aa2b04560b8bcb
Mark Ogley <phelan55@gmail.com>
2017-02-03 16:58:33 +0000

    First commit of apiTest

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,30 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    x = 2
-    y = 4
-    z = x + y
-    return ' %d what the heck!' % z
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/test/api/v1.0/tests', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks':tasks})
+
+
+#def hello_world():
+#    return 'Hello from Flask!'
 
