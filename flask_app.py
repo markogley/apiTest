88688f6b5932293cd02d70cf419c5d1857438056
Mark Ogley <phelan55@gmail.com>
2017-02-04 04:14:42 +0000

    Added Put, Delete method as well as helper method to get task uri

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response, request, url_for
 
 app = Flask(__name__)
 
@@ -20,12 +20,64 @@ tasks = [
     }
 ]
 
-@app.route('/test/api/v1.0/tests/<int:task_id>', methods=['GET'])
-def get_tasks(task_id):
+@app.route('/test/api/v1.0/tests', methods=['GET'])
+def get_tasks():
+    #task = [task for task in tasks if task['id'] == task_id]
+    #if len(task) == 0:
+    #    abort(404)
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+@app.route('/test/api/v1.0/tests', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+        }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+@app.route('/test/api/v1.0/tests/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
-    return jsonify({'tasks':task[0]})
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tests/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error':'Not Found'}), 404)
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_tasks', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
 
 
 #def hello_world():
